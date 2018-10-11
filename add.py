c292357e03d95e67f327cfc769d92db9b5a81001
hari suryawanshi <hsuryawanshi11@gmail.com>
2018-10-11 12:50:10 +0530

    two number addition

---
+++ add.py
@@ -0,0 +1,8 @@
+class test:
+        a=int(input("Enter the first number:"))
+        b=int(input("Enter the secont number:"))
+        def add():
+            c=a+b
+            print("result:",c)
+            return
+        test.add()
