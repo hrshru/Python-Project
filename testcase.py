7feb9935ca652cba89ce87c0f30b1ab432755a16
Harish <hsuryawanshi11@gmail.com>
2019-05-07 16:27:59 +0530

    commit

---
+++ testcase.py
@@ -0,0 +1,15 @@
+n=int(input())
+m=int(input())
+p=input()
+for i in range(n):
+    for j in range(m):
+        list=p.split()
+        
+    print(list)
+a=(max(list))
+    
+b=map(int, list[0])
+c=map(int, list[1])
+print(b+c)  
+z=list[2]+list[0]
+print(b)
